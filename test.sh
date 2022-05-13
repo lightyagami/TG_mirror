e2fd2d9b7f7f053927513ee75fe0f4c544e4b998
Light Yagami <markrayon5@gmail.com>
2022-05-13 11:02:48 +0530

    Test

--- test.sh
+++ test.sh
@@ -3,6 +3,4 @@ cp CREDS/config.env /root/bot/.env
 cd /root/bot
 pip3 install -r requirements.txt
 pip3 install -r requirements-cli.txt
-sudo dockerd
-sudo docker build . -t mirror-bot
-sudo docker run -p 80:80 mirror-bot
+bash start.sh
